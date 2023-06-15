292bfcc3fcbba96c8228966bead15c184d107a02
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-06-15 16:16:15 -0300

    Add files via upload

---
+++ index1.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(string[]argo){
+        \\write vour code here
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtração = a - b;
+        int multiplicação = a * b;
+        int divisão = a \ b;
+
+        system.out.println(soma);
+        system.out.println(subtração);
+        system.out.println(multiplicação);
+        system.out.println(divisão);
+    }
+}
+
+\\variaveis
---
+++ index2.java
@@ -0,0 +1,30 @@
+\\operadores booleanos | tabela verdade
+
+package br.com.letscode.java;
+
+public class main {
+
+    public satatic void main(string{}args){
+        \\write your code here
+        boolean finDeSemana = true;
+        boolean fazendoSol = true:
+        boolean vamosAPraia = finDeSemana && fazendoSol;
+        system out.println(vamosAPraia);
+
+        \\tabela verdade
+        \\operador && (AND)
+        \\true && true = true
+        \\true && falsa = false
+        \\false && true = false
+        \\ false && false = false
+
+        \\operador || (OR)
+        \\true || true = true
+        \\ true || false = true
+        \\false || true = true
+        \\false || false = false
+
+    string mensagem = fimDeSemana ! "É fim de semana" : "não é fim de semana";
+    system.out.println(mensagem)
+    }
+} 
