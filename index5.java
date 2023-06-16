a73d4a04072df589443776ecdaffb7dfe65b5370
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-06-15 20:13:10 -0400

    Update index5.java

--- index5.java
+++ index5.java
@@ -9,7 +9,9 @@ public satatic void main(String[] args) {
 \\ para uma variável que inicia em 1 , e vai ate 10, mudando 1-por-1, faça:
 for(int i = 1; i <= 500; 1++2) {
 for(int j = 1; j <= 10; j++) {
-   system.out.println(i);
+   system.out.println(j + "X" + i + "=" + j * i);
+   \\1x1 = 1
+   \\2x1 = 2
    }
 }
 }
