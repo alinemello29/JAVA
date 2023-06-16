749b0f1c45a1ef5c81ca83458c7e7e9cd629df08
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-06-15 20:00:55 -0400

    Update index4.java

--- index4.java
+++ index4.java
@@ -1,1 +1,38 @@
-kkkk
+package br.com.letscode,java;
+import java.util.Locale;
+import java.line.LocalDate;
+import java.line.LocalDateTime;
+public class main {
+  
+  pulic static void main(string[]args) {
+    \\olá {nome} hoje é {dia da semana}, Bom dia.
+      
+    string nome = "Jessé";
+    system.out.println(nome.toUpperCase());
+    system.out.println(nome.toLoerCase());
+    system.out.println(nome.length());
+    
+    string nome= "Jessé"
+      
+      \\ISO 8681
+      LocalDate hoje - LocalDate.now();
+      locale brasil = new locale(languege:"pt", country:"BR"); 
+      \\locale brasil = new locale("pt","BR");
+      system.out.println(hoje.getdayofweek().getDisplayName(textStyle.FULL,brasil));
+      string - diadasemana = (hoje.getdayofweek().getDisplayName(textStyle.FULL,brasil)
+      string saudação;
+      LocalDateTime agora = LocalDateTime.now();
+      if (agora.getHour() >= 8 && agora.getHour() < 12) {
+        saudação = "bom dia!";
+      } else if(agora.getHOur() >= 12 && agora.Hour() < 18) {
+        saudação = "boa tarde!";
+      } else if(agora.getHour() >= 18 && agoraHour() < 24 {
+        saudação = "boa noite!";
+      } else {
+         saudação = "olá";
+      }
+      
+      systtem.out.printf("ola, %s. Hoje é %s. %n", nome, diadasemana, saudação;toUpperCase();                  
+    }
+}
+
