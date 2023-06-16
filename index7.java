4dc124ab84c27495f67c46b10e331a5ff2eb138f
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-06-15 20:53:06 -0400

    Update index7.java

--- index7.java
+++ index7.java
@@ -1,1 +1,22 @@
-fvv
+package br.com.letscode.java;
+
+public class main {
+  public static void main(string[] args) {
+    string nome = "let's code"; 
+    saudação();
+  }
+    public satatic void saudação() {
+      system.out.println("hello" + "nome");
+   }
+}
+
+public class main {
+  public static void main(string[] args) {
+    int resultado = soma(a:2, b:3);
+    system.out.println(resultado):
+  }
+  
+  public static int soma(int a, int b) {
+    return a + B;
+  }
+}
