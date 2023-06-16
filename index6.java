3b764410ab50da0a5e4edf674564e1c7de7396c4
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-06-15 20:40:30 -0400

    Update index6.java

--- index6.java
+++ index6.java
@@ -1,1 +1,68 @@
-fddv
+vetores
+
+package br.com.letscode.java;
+
+public class main {
+  public static void main(string[] args) {
+    int numero1 = 1;
+    int numero1 = 2;
+    int numero1 = 3;
+    int numero1 = 4;
+    int numero1 = 5;
+    
+    int[] nuemros = new int[5];
+    \\[0] [1] [2] [3] [4]
+    numero[0] = 1;
+    numero[1] = 2;
+    numero[2] = 3;
+    numero[3] = 4;
+    numero[4] = 5;
+    system.out.println(numero);
+    for (int i=0; < numero.length; i++) {
+      system.out.println(numero[i]);
+     
+    int[] letras = new int[5];
+    \\[0] [1] [2] [3] [4] 
+    letras[0] = "A";
+    letras[1] = "B";
+    letras[2] = "C";
+    letras[3] = "D";
+    letras[4] = "E";
+    system.out.println(numero);
+    for (int i=0; < letras.length; i++) {
+      system.out.println(letras[i]);
+      
+    
+    string letras = { "A", "B", "C", "j", "x'};
+      for (int i=0; i < letras.length; i++) {
+        system.out.println(letras[i]);
+    }
+                     
+     system.out.println(Arrays.toString(letras));
+     
+                     
+     # 2 exercicio 
+     public class main {
+           public static void main(string[] args) {  
+            int[] numeros = {9, 10, 12, 25, 2}
+             int maior = numeros[0];
+             int menos = numeros[0];
+             int media;
+             
+             for(int i=0; i < numeros.length; i++) {
+               if(numero[i] > maior) {
+                 maior = numeros[i];
+               }
+               if (numero[i] < menor) {
+                 menor = numero[i];
+               }
+               media = media + numeros[i];
+             }
+            system.out.println("maior:" + maior);
+            system.out.println("menor:" + menor);
+            system.out.println("media:" + media\numeros.length);
+     
+  }
+}
+
+      
