adf701cd8bd8cdf2c8037caa08aba6244b8fafdd
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-06-15 20:11:12 -0400

    Update index5.java

--- index5.java
+++ index5.java
@@ -1,1 +1,15 @@
-fggt
+\\laços numéricos
+
+packege br.com.lestcode.java
+
+public class main {
+
+public satatic void main(String[] args) {
+\\ 1 2 3 4 5 6 7 8 9 10
+\\ para uma variável que inicia em 1 , e vai ate 10, mudando 1-por-1, faça:
+for(int i = 1; i <= 500; 1++2) {
+for(int j = 1; j <= 10; j++) {
+   system.out.println(i);
+   }
+}
+}
